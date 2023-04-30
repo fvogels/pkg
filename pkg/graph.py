eb6cdaf3fddcf82d34e0917ac1d060d5dc62831c
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-30 15:26:54 +0200

    redesign of Graph

--- pkg/graph.py
+++ pkg/graph.py
@@ -11,16 +11,30 @@ class Graph:
         self.__graph = db.graph
 
     def find_one(self, **kwargs):
-        return self.__graph.find_one(kwargs)
+        raw_node = self.__graph.find_one(kwargs)
+        if raw_node:
+            return Node(raw_node)
+        else:
+            return None
 
     def find(self, **kwargs):
-        return self.__graph.find(kwargs)
+        return map(Node, self.__graph.find(kwargs))
+
+    def all_nodes(self):
+        return self.find()
 
 
-class NodeId:
-    def __init__(self, id):
-        self.__id = id
+
+class Node:
+    def __init__(self, raw_node):
+        self.__raw_node = raw_node
 
     @property
     def id(self):
-        return self.__id
+        return self.__raw_node['_id']
+
+    def __getattr__(self, attribute):
+        if attribute in self.__raw_node:
+            return self.__raw_node[attribute]
+        else:
+            raise AttributeError()
