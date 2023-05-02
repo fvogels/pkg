fe463e005bfa5319540a8c2b4780ae70596ac905
Frederic Vogels <frederic.vogels@ucll.be>
2023-05-02 11:08:50 +0200

    verify command

--- pkg.py
+++ pkg.py
@@ -1,4 +1,5 @@
 import click
+from functools import reduce
 from pkg.graph import Graph
 from pkg.visual import generate_html
 from pkg.protocols import apply_protocol
@@ -24,19 +25,26 @@ def fetch(id):
 
 
 @cli.command()
-@click.option('--name')
-def find(name):
+@click.argument('names', nargs=-1)
+def find(names):
     graph = Graph()
-    kwargs = {}
-    if name:
-        kwargs['name'] = { '$regex': name, '$options': 'i' }
-    nodes = graph.find(**kwargs)
+    nodesets = [
+        {
+            descendant
+            for node in graph.find_on_name(name)
+            for descendant in node.descendants
+        }
+        for name in names
+    ]
+    print(nodesets)
+    intersection = reduce(lambda x, y: x & y, nodesets)
+
     console = Console()
-    for node in nodes:
+    for node in intersection:
         table = Table(show_header=False)
         table.add_column('Key', style='bold')
         table.add_column('Value')
-        for key, value in node.items():
+        for key, value in node.fields():
             table.add_row(key, str(value))
         console.print(table)
 
@@ -48,6 +56,17 @@ def graph():
         html = generate_html(graph)
         print(html, file=file)
     webbrowser.open(file.name)
+
+
+@cli.command()
+def verify():
+    graph = Graph()
+    nodes = graph.all_nodes()
+    for node in nodes:
+        for child in node.children:
+            for grandchild in child.children:
+                if grandchild in node.children:
+                    print(f'{node.name} -> {child.name} -> {grandchild.name}')
 
 
 if __name__ == '__main__':
